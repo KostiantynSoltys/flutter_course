98a05a82e65fb7cb1a9f78405990a5e5cc504156
Constantin Soltys <ksoltys@fondy.eu>
2023-09-23 20:08:21 +0300

    Music collection himework

--- README.md
+++ README.md
@@ -1,16 +1,17 @@
 # flutter_course
 
-A new Flutter project.
+Робоча назва застосунку Art of Ukraine
 
-## Getting Started
 
-This project is a starting point for a Flutter application.
+Кожен день в ньому буде невеличкий допис про одну з персоналій українського мистецтва, такі собі “картки митця”. Наприклад, картина Марії Примаченко, внизу трошки цікавого про картину, потім трошки цікавого про митця. Наступного дня вже буде інша персоналія. Справа не буде обмежуватись художниками, плануються й композитори, співакі, актори, режисери і т.і. Після невеликих абзаців з цікавими фактами про картину чи митця буде пару лінків на ресурси з більш детальною біографією та посилання на творчість, в залежності від роду діяльності: ютьюб, сайти з картинами, посилання на музику в стрімінгах. Також буде можливість перейти на картки за попередні дні, свайпом вправо або через кнопку. На екрані з карткою митця буде можливість поставити лайк, що додасть карту в розділ “Залайкане”, де люди швидко зможуть знайти те, що вони особливо відмітили. Також біля іконки з лайком буде лічильник з кількістю усіх лайків, поставлених даній картці. Можливо є сенс прикрутити можливість поділитись карткою через месенджери та соцмережі, але це не точно)
 
-A few resources to get you started if this is your first Flutter project:
 
-- [Lab: Write your first Flutter app](https://docs.flutter.dev/get-started/codelab)
-- [Cookbook: Useful Flutter samples](https://docs.flutter.dev/cookbook)
 
-For help getting started with Flutter development, view the
-[online documentation](https://docs.flutter.dev/), which offers tutorials,
-samples, guidance on mobile development, and a full API reference.
+Working title of the application is the Art of Ukraine
+
+Every day there will be a small post about one of the personalities of Ukrainian art, such "cards of the artist". For example, a painting by Maria Prymachenko, at the bottom will be a bit of interesting information about the painting, then some curious facts about the artist. The next day there will be a different personality. It’s not only about painting, artists, composers, singers, actors, directors, etc. are also planned. After small paragraphs with interesting facts about the painting or the artist, there will be a couple of links to resources with a more detailed biography and links to artist’s works, depending on the type of activity: YouTube, sites with pictures, links to music in stream services. It will be  also possible to switch to cards of previous days by swiping to the right or using the button. There will be also an option to like the artist card screen, this action will add the card to the Liked section, where people can quickly find what they've liked. Also, next to the like icon, there will be a counter with the number of all likes given to this card. Maybe it makes sense to create an ability to share the card through messengers and social networks, but it I’m no sure)
+
+Project scheme:
+Блок-схема:         https://excalidraw.com/#json=ij3zXfHYbay0vAgxKyojD,zglRefG0EJbZoTbraNbMnA
+
+GIF з домашньої роботи: https://ezgif.com/video-to-gif/ezgif-3-cf4d94a44c.mov
---
+++ lib/homework/music_collection.dart
@@ -0,0 +1,185 @@
+//Song class
+class Song {
+  const Song(
+      {required this.name,
+      required this.artist,
+      required this.duration,
+      required this.year});
+
+  final String name;
+  final String artist;
+  final String duration;
+  final String year;
+}
+
+//Playlist class with display method that output formatted playlist
+class Playlist with SearchMixin {
+  const Playlist({required this.songs, required this.title});
+  final String title;
+  final List<Song> songs;
+  void display() {
+    if (songs.isEmpty) {
+      print('Your \'$title\' playlist is empty');
+    } else {
+      print('Playlist\n${title.toUpperCase()}\n');
+      for (final song in songs) {
+        print(
+            '\'${song.name}\' by ${song.artist} ${song.duration} ${song.year}');
+      }
+      print(songs.totalTime());
+    }
+  }
+}
+
+//Mixin to search Songs in Playlist by song or artist name
+mixin SearchMixin {
+  void searchByArtistOrSong(input) {
+    List<Song> searchResult = myPlaylist[0]
+        .songs
+        .where((t) => t.artist == input || t.name == input)
+        .toList();
+    if (searchResult.isEmpty) {
+      print('No songs found');
+    } else {
+      print('In ${myPlaylist[0].title.toUpperCase()} playlist found:\n');
+      for (final song in searchResult) {
+        print(
+            '\'${song.name}\' by ${song.artist} ${song.duration} ${song.year}');
+      }
+      print(searchResult.totalTime());
+    }
+  }
+}
+
+//Extension for List<Song> class to get total time of selected songs
+extension MyExtension on List<Song> {
+  String totalTime() {
+    int result = 0;
+    for (final song in this) {
+      List songLength = song.duration.split(':');
+      int songInSeconds =
+          int.parse(songLength[0]) * 60 + int.parse(songLength[1]);
+      result += songInSeconds;
+    }
+    String text;
+    int minutes = result ~/ 60;
+    int seconds = result % 60;
+    if (minutes > 60) {
+      int hours = minutes ~/ 60;
+      minutes = minutes % 60;
+      text =
+          '\nTotal duration $hours:$minutes:${seconds < 10 ? '0$seconds' : '$seconds'}\n';
+    } else {
+      text =
+          '\nTotal duration $minutes:${seconds < 10 ? '0$seconds' : '$seconds'}\n';
+    }
+    return text;
+  }
+}
+
+//List of Songs
+const mySongs = [
+  Song(
+      name: 'Instrumental',
+      artist: 'Black Country, New Road',
+      duration: '5:27',
+      year: '2021'),
+  Song(
+      name: 'Athens, France',
+      artist: 'Black Country, New Road',
+      duration: '6:23',
+      year: '2021'),
+  Song(
+      name: 'Science Fair',
+      artist: 'Black Country, New Road',
+      duration: '6:20',
+      year: '2021'),
+  Song(
+      name: 'Sunglasses',
+      artist: 'Black Country, New Road',
+      duration: '9:51',
+      year: '2021'),
+  Song(
+      name: 'Track X',
+      artist: 'Black Country, New Road',
+      duration: '4:44',
+      year: '2021'),
+  Song(
+      name: 'Opus',
+      artist: 'Black Country, New Road',
+      duration: '8:01',
+      year: '2021'),
+  Song(name: 'G.S.K.', artist: 'Squid', duration: '3:11', year: '2021'),
+  Song(name: 'Narrator', artist: 'Squid', duration: '8:29', year: '2021'),
+  Song(name: 'Boy Racers', artist: 'Squid', duration: '7:34', year: '2021'),
+  Song(name: 'Padding', artist: 'Squid', duration: '6:17', year: '2021'),
+  Song(
+      name: 'Documentary Filmmaker',
+      artist: 'Squid',
+      duration: '4:56',
+      year: '2021'),
+  Song(name: '2010', artist: 'Squid', duration: '4:28', year: '2021'),
+  Song(name: 'The Flyover', artist: 'Squid', duration: '1:11', year: '2021'),
+  Song(name: 'Peel St.', artist: 'Squid', duration: '4:52', year: '2021'),
+  Song(name: 'Global Groove', artist: 'Squid', duration: '5:08', year: '2021'),
+  Song(name: 'Pamphlets', artist: 'Squid', duration: '8:04', year: '2021'),
+  Song(
+      name: 'Holiday Destination',
+      artist: 'Nadine Shah',
+      duration: '5:39',
+      year: '2017'),
+  Song(name: '2016', artist: 'Nadine Shah', duration: '4:47', year: '2017'),
+  Song(
+      name: 'Out the Way',
+      artist: 'Nadine Shah',
+      duration: '3:48',
+      year: '2017'),
+  Song(name: 'Evil', artist: 'Nadine Shah', duration: '5:00', year: '2017'),
+  Song(
+      name: 'Aching Bones',
+      artist: 'Nadine Shah',
+      duration: '3:55',
+      year: '2013'),
+  Song(name: 'Trad', artist: 'Nadine Shah', duration: '5:28', year: '2020'),
+  Song(
+      name: 'Kitchen Sink',
+      artist: 'Nadine Shah',
+      duration: '3:27',
+      year: '2020'),
+  Song(
+      name: 'Balance is Gone',
+      artist: 'Billy Nomates',
+      duration: '3:34',
+      year: '2023'),
+  Song(
+      name: 'Emergency Telephone',
+      artist: 'Billy Nomates',
+      duration: '4:08',
+      year: '2021'),
+  Song(
+      name: 'Supermarket Sweep (feat. Jason Williamson)',
+      artist: 'Billy Nomates',
+      duration: '2:59',
+      year: '2021'),
+  Song(
+      name: 'Mork n Mindy (feat. Billy Nomates)',
+      artist: 'Sleaford Mods',
+      duration: '3:24',
+      year: '2021'),
+  Song(
+      name: 'Nudge It (feat. Amy Taylor)',
+      artist: 'Sleaford Mods',
+      duration: '3:43',
+      year: '2021'),
+  Song(name: 'Ducter', artist: 'black midi', duration: '6:42', year: '2019'),
+];
+
+//Playlist
+const myPlaylist = [Playlist(title: 'Current Mood', songs: mySongs)];
+
+void main() {
+  //this line is search by artists or song
+  myPlaylist[0].searchByArtistOrSong('Nadine Shah');
+  //and this one id about formatted lpaylist output
+  myPlaylist[0].display();
+}
